6b44276c0c103531c2aa0e5c42a38a60640065e9
João Marco <moaojarco@gmail.com>
2024-03-10 11:11:03 -0300

    Update golang script

--- scripts/arch-go/main.go
+++ scripts/arch-go/main.go
@@ -4,57 +4,77 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-  "time"
+	"time"
 )
 
+var logo = `
+    ⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⣤⣤⡄⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠔⠓⠄⣀⣿⣿⣧⠄⣀⣍⡂⠀⡸⣿⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⣿⣿⠟⠀⠠⣡⣶⠿⠿⠿⣎⣻⣿⣇⠾⠟⠿⢿⣧⢱⡸⣿⣿⣿⣿⣿⣿ 
+    ⣿⣿⣿⠿⠃⠀⢀⣴⠟⠀⢀⣀⡀⠈⢿⣿⠁⠀⣀⡀⠀⠹⣷⣷⣶⣶⣬⣝⢿⣿ 
+    ⣿⡿⢣⣾⣿⣄⢸⣿⡀⠀⠘⠛⠃⠀⣸⣿⡀⠀⠛⠃⠀⢠⣿⣿⣿⣿⣿⣿⣦⢻ 
+    ⣿⢣⣿⣿⣿⣿⣿⣿⣷⣤⣤⣤⣤⣼⣿⣿⣷⣤⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⡝ 
+    ⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢁ 
+    ⣿⣷⣬⣭⣥⣶⣬⣙⡛⠻⢿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠈⠻⣿⣿⣿⣿⣿⣿⡟⣸ 
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣬⣍⣙⠻⠿⠿⠀⠀⠀⠀⢻⣿⣿⣿⠿⢋⣼⣿ 
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⣀⣬⣭⣤⣶⣶⣿⣿⣿
+    yay Dotfiles v0.0.1
+
+`
+
 var pacotes = []string{
-  "base-devel",
+	"base-devel",
 	"bitwarden",
 	"exa",
 	"neofetch",
-	"kitty",
+	"alacritty",
 	"rustup",
 	"tmux",
-  "git",
-  "go",
-  "neovim",
-  "adobe-source-han-sans-cn-fonts",
-  "adobe-source-han-sans-jp-fonts",
-  "adobe-source-han-sans-kr-fonts",
-  "adobe-source-han-sans-otc-fonts",
-  "adobe-source-han-sans-tw-fonts",
-  "firefox",
+	"git",
+	"go",
+	"neovim",
+	"adobe-source-han-sans-cn-fonts",
+	"adobe-source-han-sans-jp-fonts",
+	"adobe-source-han-sans-kr-fonts",
+	"adobe-source-han-sans-otc-fonts",
+	"adobe-source-han-sans-tw-fonts",
+	"firefox",
 }
 
-func main() {
-  for _, pacote := range pacotes {
-    if !is_installed(pacote) {
-      install_package(pacote)
-    } else {
-      fmt.Printf("%s já está instalado ✅\n", pacote)
-      time.Sleep(1 * time.Second)
 
-    }
-  }
+func main() {
+  fmt.Printf("%s", logo)
+
+	for _, pacote := range pacotes {
+		if !is_installed(pacote) {
+			install_package(pacote)
+		} else {
+			fmt.Printf("%s já está instalado ✅\n", pacote)
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
 func is_installed(packageName string) bool {
-  cmd := exec.Command("pacman", "-Q", packageName)
+	cmd := exec.Command("pacman", "-Q", packageName)
 
-  if err := cmd.Run(); err != nil {
-    return false
-  }
-  return true
+	if err := cmd.Run(); err != nil {
+		return false
+	}
+	return true
 }
 
 func install_package(packageName string) {
-  fmt.Printf("Instalando %s...", packageName)
-  cmd := exec.Command("sudo", "pacman", "-S", "--noconfirm", packageName)
-  cmd.Stdout = os.Stdout
-  cmd.Stderr = os.Stderr
-  
-  if err :=  cmd.Run(); err != nil {
-    fmt.Printf("Erro ao instalar %s: %v", packageName, err)
-    os.Exit(1)
-  }
+	fmt.Printf("Instalando %s...", packageName)
+	cmd := exec.Command("sudo", "pacman", "-S", "--noconfirm", packageName)
+  exec.Command("clear")
+  fmt.Printf("%s", logo)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Erro ao instalar %s: %v", packageName, err)
+		os.Exit(1)
+	}
+
 }
