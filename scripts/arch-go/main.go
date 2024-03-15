de817b0c11159bed97b53153544054716471baf7
Joao Marco <moaojarco@gmail.com>
2024-03-15 16:37:48 -0300

    feat: Install LunarVim, Install Yay, add more packages

--- scripts/arch-go/main.go
+++ scripts/arch-go/main.go
@@ -17,7 +17,7 @@ var logo = `
     ⣿⣷⣬⣭⣥⣶⣬⣙⡛⠻⢿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠈⠻⣿⣿⣿⣿⣿⣿⡟⣸ 
     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣬⣍⣙⠻⠿⠿⠀⠀⠀⠀⢻⣿⣿⣿⠿⢋⣼⣿ 
     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣄⡀⠀⣀⣬⣭⣤⣶⣶⣿⣿⣿
-    yay Script v0.0.1
+    yay Script v0.0.2
 `
 
 var pacotes = []string{
@@ -27,6 +27,12 @@ var pacotes = []string{
 	"adobe-source-han-sans-otc-fonts",
 	"adobe-source-han-sans-tw-fonts",
 	"alacritty",
+	"bitwarden",
+	"pulseaudio",
+	"pulseaudio-alsa",
+	"alsa-utils",
+	"lightdm",
+	"lightdm-gtk-greeter",
 	"base-devel",
 	"curl",
 	"exa",
@@ -41,6 +47,7 @@ var pacotes = []string{
 	"pacman-contrib",
 	"polybar",
 	"picom",
+	"openssh",
 	"ranger",
 	"rustup",
 	"tmux",
@@ -59,12 +66,18 @@ var pacotes = []string{
 func main() {
 	fmt.Printf("%s", logo)
 
+  utils.InstallYay()
+  fmt.Println("YAY INSTALLED")
+  time.Sleep(1 * time.Second)
+  utils.InstallLunarVim()
+
 	for _, pacote := range pacotes {
 		if !utils.IsInstalled(pacote) {
 			utils.InstallPackage(pacote)
 		} else {
 			fmt.Printf("%s is already installed. ✅\n", pacote)
-			time.Sleep(1 * time.Second)
+			time.Sleep(300 * time.Millisecond)
 		}
 	}
+
 }
--- scripts/arch-go/utils/utils.go
+++ scripts/arch-go/utils/utils.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-  "time"
+	"strings"
+	"time"
 )
 
 func IsInstalled(packageName string) bool {
@@ -30,4 +31,59 @@ func InstallPackage(packageName string) {
 		fmt.Printf("Error while try install %s: %v", packageName, err)
 		os.Exit(1)
 	}
+}
+
+func InstallLunarVim() {
+	fmt.Println("Installing LunarVim")
+	cmd := exec.Command("sudo", "bash", "-c", `LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)`)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+
+	if err != nil {
+		return
+	}
+}
+
+func InstallYay() error {
+	if _, err := exec.LookPath("git"); err != nil {
+		return fmt.Errorf("git não encontrado no sistema")
+	}
+
+	if err := exec.Command("git", "clone", "https://aur.archlinux.org/yay.git").Run(); err != nil {
+		return fmt.Errorf("falha ao clonar o repositório yay: %v", err)
+	}
+
+	if err := os.Chdir("yay"); err != nil {
+		fmt.Println("KKKKKKKKK")
+		return fmt.Errorf("falha ao acessar o diretório yay: %v", err)
+	}
+
+	makeCmd := exec.Command("makepkg", "-si")
+	makeCmd.Stdout = os.Stdout
+	makeCmd.Stderr = os.Stderr
+
+	// Redireciona a entrada padrão para responder automaticamente com "Y" (sim) ao prompt
+	makeCmd.Stdin = strings.NewReader("Y\n")
+
+	if err := makeCmd.Run(); err != nil {
+		return fmt.Errorf("falha ao compilar e instalar yay: %v", err)
+	}
+
+	if err := os.Chdir(".."); err != nil {
+		return fmt.Errorf("Falha ao retornar para o diretorio anterior: %v", err)
+	}
+
+	if err := os.RemoveAll("yay"); err != nil {
+		return fmt.Errorf("falha ao remover a pasta yay: %v", err)
+	}
+
+	return nil
+}
+
+func requireRoot() error {
+	if os.Geteuid() != 0 {
+		return fmt.Errorf("You're useless, execute as sudo")
+	}
+	return nil
 }
